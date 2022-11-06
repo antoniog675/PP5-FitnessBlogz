d6316a755b0e2003dcd1f6ca1698bdee6b23940a
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-11-06 20:11:44 +0000

    navbar links working, hover features implemented

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,19 +2,22 @@ import React from 'react'
 import { Container, Navbar, Nav } from 'react-bootstrap'
 import logo from '../assets/logo.jpg'
 import styles from "../styles/NavBar.module.css"
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
     <Container>
-        <Navbar.Brand className="col-2"><img src={logo} alt='website logo' height="75" /></Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand className="col-2"><img src={logo} alt='website logo' height="75" /></Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle className="navbar-dark" aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="white" id="basic-navbar-nav">
-            <Nav.Link className="text-center" ><h1>FitnessBlogz </h1></Nav.Link>
+            <NavLink to="/" className={styles.NavBarTitle} ><h1>FitnessBlogz </h1></NavLink>
             <Nav className="ms-auto text-center">
-                <Nav.Link className={styles.NavBarContent}> <i className="fas fa-home"></i> Home</Nav.Link>
-                <Nav.Link className={styles.NavBarContent}> <i className="fas fa-sign-in-alt"></i> Sign in</Nav.Link>
-                <Nav.Link className={styles.NavBarContent}><i className="fas fa-user-plus"></i> Sign up</Nav.Link>
+                <NavLink exact to="/" className={styles.NavBarContent} activeClassName={styles.Active}> <i className="fas fa-home"></i> Home</NavLink>
+                <NavLink to="/signin" className={styles.NavBarContent} activeClassName={styles.Active}> <i className="fas fa-sign-in-alt"></i> Sign in</NavLink>
+                <NavLink to="/signup" className={styles.NavBarContent} activeClassName={styles.Active}><i className="fas fa-user-plus"></i> Sign up</NavLink>
             </Nav>
         </Navbar.Collapse>
     </Container>

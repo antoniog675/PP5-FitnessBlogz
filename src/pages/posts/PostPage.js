de03515675fe81c2df27b95fa401daef474d8563
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-11-11 14:43:50 +0000

    users able to edit and delete comments, infinite scroll implemented

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -12,6 +12,10 @@ import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext"
 import Comment from "../comments/Comment";
 
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
+
 function PostPage() {
     const {id} = useParams();
     const [post, setPost] = useState({ results: [] })
@@ -53,13 +57,20 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
-          {comments.results.length ? (
-            comments.results.map(comment => (
-              <Comment key={comment.id} {...comment}
-              setPost={setPost}
-              setComments={setComments}
+            {comments.results.length ? (
+              <InfiniteScroll 
+                children={comments.results.map((comment) => (
+                <Comment 
+                key={comment.id} {...comment}
+                setPost={setPost}
+                setComments={setComments}
+                />
+                ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
               />
-            ))
           ) : currentUser ? (
             <span>No comments yet</span>
           ) : (

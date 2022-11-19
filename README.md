ff882f8cd8e214bc2a7afc13d64cc83f9bfd68a3
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-11-19 10:26:55 +0000

    pushing to readme

--- README.md
+++ README.md
@@ -1,11 +1,150 @@
-# pp5-fitnessblogz
+# FitnessBlogz
 
-Fonts: 
-Fonts used for this project are 'Josefin Sans' for the title and sub-headings of the website, the 2nd font used is 'Exo' which is going to be used for users description, and other paragraphs
+## About the website - 
+<hr>
 
-Images: From cloudinary sample pictures, the form picture
+![Am I Responsive image](/src/assets/pp5_amiresponsive.jpg)
+
+This website is designed to be an Instagram like page where users post pictures, put a title and description on their posts. They will also be able to like, comments on posts and also be able to follow and unfollow profiles.
+
+This site is targeted for people who are into fitness, for users who like to look for inspiration from other people working towards the same goal. Users will also be able to share their stories and their journey towards a healthier lifestyle, users can also share different meals they are having that is full of protein, high in nutrients and low on carbs.
+
+[- Live Website -](https://fitness-blogz.herokuapp.com/)
+
+[- API Repo -](https://github.com/antoniog675/drf-api-pp5)
+
+<hr>
+
+## Contents -
+
+1.[User Experience](#user-experience)
+
+2.[Features](#features)
+
+3.[Technologies Used](#technologies-used)
+
+4.[Testing](#testing)
+
+5.[Deployment](#deployment)
+
+6.[Finished Product](#finished-product)
+
+7.[Credits](#credits)
+
+8.[Bugs](#bugs)
+
+9.[Acknowledgments](#acknowledgments)
+
+<hr>
+
+## User Experience
+
+### User Stories -
+- Insert all user stories
+
+### Structure -
+#### ERD - 
+- 
+#### Data model
+
+### Skeleton -
+#### Wireframe -
+- Desktop
+    - Feed
+    - Sign in
+    - Sign up
+    - Create post
+    - Edit and delete post
+    - Edit profile
+    - Edit username
+    - Edit password
+- Mobile
+    - Feed
+    - Sign in
+    - Sign up
+    - Create post
+    - Edit and delete post
+    - Edit profile
+    - Edit username
+    - Edit password
+
+### Surface -
+#### Color scheme -
+- The website is going to have a very simple design, black, white, and grey.
+- The Navbar is going to have a black background design with fonts within  being white, so that there are no issues with contrast
+- Sign in and Sign up buttons are going to be black with a white font, when hovered over the colours will invert.
+-Apart from that the buttons for adding comments, posts and edits will be grey with white fonts, and when hovered over the colours will invert
+#### Fonts -
+- Fonts used for this project are 'Josefin Sans' for the title and sub-headings of the website
+
+- The 2nd font used is 'Exo' which is going to be used for users description, and other paragraphs
+
+
+<hr>
+
+## Features
+
+### Feed
+
+### Navbar
+
+### Search bar
+
+### Sign up and Sign in
+
+### Create, edit and delete posts
+
+### Create, edit and delete comments
+
+### Like and comment features
+
+<hr>
+
+## Technologies Used
+
+<hr>
 
 ## Testing 
 
+- Navbar testing
+
+- Testing for users sign in and sign up
+
+- Does the correct post load up when selected
+
+- Does the correct profile load up when selected
+
+- Number of likes and comments correct 
+
+### Website efficiency
+
+### Use ESlint to rest the whole website and individual files?
+
+### W3C for CSS validation
+
 Do the components within the navbar render?
 Does the navbar change according to whether the user is signed in or no?
+
+<hr>
+
+## Deployment
+
+<hr>
+
+## Finished Product
+
+<hr>
+
+## Credits
+
+<hr>
+
+## Bugs
+
+<hr>
+
+## Acknowledgments
+
+<hr>
+
+Images: From cloudinary sample pictures, the form picture
--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -13,77 +13,77 @@ export const useCurrentUser = () => useContext(CurrentUserContext)
 export const useSetCurrentUser = () => useContext(SetCurrentUserContext)
 
 export const CurrentUserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const history = useHistory();
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
 
-    const handleMount = async () => {
-        try {
-            const { data } = await axiosRes.get('dj-rest-auth/user/')
-            setCurrentUser(data)
+  const handleMount = async () => {
+    try {
+      const { data } = await axiosRes.get('dj-rest-auth/user/')
+      setCurrentUser(data)
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
+  // Cache the results between re-renders
+
+  useMemo(() => {
+    axiosReq.interceptors.request.use(
+      async (config) => {
+        if (shouldRefreshToken()) {
+          try {
+            await axios.post('/dj-rest-auth/token/refresh/')
           } catch (err) {
-            console.log(err);
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                history.push('/signin')
+              }
+              return null
+            });
+            removeTokenTimestamp();
+            return config;
+          }
         }
-    };
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
+    );
 
-    useEffect(() => {
-      handleMount();
-    }, []);
+    // Get the response from the api
 
-    // Cache the results between re-renders
-
-    useMemo(() => {
-        axiosReq.interceptors.request.use(
-            async (config) => {
-                if (shouldRefreshToken()) {
-                    try {
-                      await axios.post('/dj-rest-auth/token/refresh/')
-                    } catch (err) {
-                        setCurrentUser((prevCurrentUser) => {
-                            if (prevCurrentUser) {
-                                history.push('/signin')
-                              }
-                            return null 
-                            });
-                        removeTokenTimestamp();
-                        return config;
-                      }
-                }
-              return config;
-            },
-            (err) => {
-              return Promise.reject(err);
-            }
-        );
-      
-      // Get the response from the api
-
-      axiosRes.interceptors.response.use(
-          (response) => response,
-            async (err) => {
-                if (err.response?.status === 401) {
-                    try {
-                        await axios.post('/dj-rest-auth/token/refresh/')
-                      } catch (err) {
-                        setCurrentUser((prevCurrentUser) => {
-                          if (prevCurrentUser) {
-                            history.push('/signin')
-                          }
-                        return null;
-                      });
-                    removeTokenTimestamp();
-                    };
-                    return axios(err.config)
-                }
-                return Promise.reject(err)
-            }
-        )
-    }, [history]);
-
-    return (
-        <CurrentUserContext.Provider value={currentUser}>
-            <SetCurrentUserContext.Provider value={setCurrentUser}>
-                {children}
-            </SetCurrentUserContext.Provider>
-        </CurrentUserContext.Provider>
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401) {
+          try {
+            await axios.post('/dj-rest-auth/token/refresh/')
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                history.push('/signin')
+              }
+              return null;
+            });
+            removeTokenTimestamp();
+          };
+          return axios(err.config)
+        }
+        return Promise.reject(err)
+      }
     )
+  }, [history]);
+
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        {children}
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
+  )
 }
